8d8e5d7ff3fb55d6168d127d0f1715bd33923c15
PaoloTolentino <tolentinopaolomatthew23@gmail.com>
2026-02-03 13:26:59 +0800

    chore: bump version to 3.1.0

--- apps/native/app.config.ts
+++ apps/native/app.config.ts
@@ -18,7 +18,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   newArchEnabled: true,
   name: getAppName(),
   slug: "pmgt-flow",
-  version: "3.0.1",
+  version: "3.1.0",
   orientation: "landscape",
   icon: "./assets/app-icon.png",
   userInterfaceStyle: "light",
